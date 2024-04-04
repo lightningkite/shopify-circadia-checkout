0a2b6d55f6244623be67b656dfb0a5d6a2b24c0c
Bradley Leavitt <phoof05@gmail.com>
2024-04-04 15:16:45 -0600

    giving good placement to error banner

--- circadia-checkout/extensions/lk-checkout-ui/src/Checkout.tsx
+++ circadia-checkout/extensions/lk-checkout-ui/src/Checkout.tsx
@@ -1,20 +1,19 @@
 import {
   reactExtension,
   useBuyerJourneyIntercept,
-  useExtensionCapability,
   useShippingAddress,
   Banner,
 } from "@shopify/ui-extensions-react/checkout";
 import { useEffect, useState } from "react";
 
-export default reactExtension("purchase.checkout.block.render", () => (
-  <RequireInfoExtension />
-));
+export default reactExtension(
+  "purchase.checkout.delivery-address.render-before",
+  () => <RequireInfoExtension />,
+);
 
 function RequireInfoExtension() {
   const [validationError, setValidationError] = useState("");
   const shippingAddress = useShippingAddress();
-  const canBlockProgress = useExtensionCapability("block_progress");
 
   useBuyerJourneyIntercept(({ canBlockProgress }) => {
     const errors = [];
@@ -40,7 +39,7 @@ function RequireInfoExtension() {
         perform: (result) => {
           // If progress can be blocked, then set a validation error on the custom field
           if (result.behavior === "block") {
-            setValidationError(errors.join("\n"));
+            setValidationError(errors.join(", "));
           }
         },
       };
@@ -61,9 +60,11 @@ function RequireInfoExtension() {
 
   return (
     <>
-      {validationError && <Banner title="Missing Required Information" status="critical">
-        {`The following information needs to be entered: ${validationError}`}
-      </Banner>}
+      {validationError && (
+        <Banner title="Missing Required Information" status="critical">
+          {`The following information needs to be entered: ${validationError}`}
+        </Banner>
+      )}
     </>
   );
 }
